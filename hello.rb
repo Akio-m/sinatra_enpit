16a19352ed980132f51b6f759b3b03c7ee6b4bac
Akio-m <k168577@ie.u-ryukyu.ac.jp>
2016-09-19 05:06:50 +0000

    add html tags

--- hello.rb
+++ hello.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
 get '/' do
-  "<h1>Hell World!</h1>"
+<<EOS
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>practice</title>
+  </head>
+
+  <body>
+    <h1>Hell World!</h1><br>
+    fire!!<br>
+  </body>
+</html>
+EOS
 end
